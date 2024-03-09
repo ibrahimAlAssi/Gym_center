6081d91010a2184f90d60f8bd87546b34414f0d8
Bashar <bsharbkayt@gmail.com>
2024-03-09 17:40:43 +0300

    feat: add seeders

---
+++ database/seeders/Domains/Club/ContactSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders\Domains\Club;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use Illuminate\Database\Seeder;
+use App\Domains\Club\Models\Contact;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // Contact::factory()->for($gym)->create();
+    }
+}
