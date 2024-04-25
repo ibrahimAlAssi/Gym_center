7d6c25be72bd3934acb3023517b5ec7cfb8fbac5
Bashar <bsharbkayt@gmail.com>
2024-04-25 03:12:08 +0300

    feat:add pusher

--- app/Domains/Entities/Models/Coach.php
+++ app/Domains/Entities/Models/Coach.php
@@ -46,6 +46,10 @@ class Coach extends Authenticatable implements HasMedia
     {
         return $this->hasMany(Chat::class);
     }
+    public function players(): HasMany
+    {
+        return $this->hasMany(Player::class);
+    }
 
     public function gym(): BelongsTo
     {
