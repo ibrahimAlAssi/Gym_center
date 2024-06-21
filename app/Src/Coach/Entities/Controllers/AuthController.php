bee7b6b4c02e8bf2cece1f394c4ebe98047b9a97
Ibrahim Al Assi <ibrahimalassi8@gmail.com>
2024-06-21 18:42:49 +0300

    update reset password

--- app/Src/Coach/Entities/Controllers/AuthController.php
+++ app/Src/Coach/Entities/Controllers/AuthController.php
@@ -12,7 +12,9 @@ use Carbon\Carbon;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
 
 class AuthController extends Controller
@@ -82,19 +84,29 @@ class AuthController extends Controller
             'password' => 'nullable|string|min:8',
         ]);
 
-        $resetCode = ResetCodePassword::where('email', $request->email)
-            ->where('created_at', '>=', Carbon::now()->subMinutes(5)->toDateTimeString())->first();
-        if (! $resetCode || $resetCode->code != $request->code) {
-            return $this->failedResponse(message: __('passwords.invalid_code'));
+        try {
+            $resetCode = ResetCodePassword::where('email', $request->email)
+                ->where('created_at', '>=', Carbon::now()->subMinutes(5)->toDateTimeString())->first();
+            if (! $resetCode || $resetCode->code != $request->code) {
+                return $this->failedResponse(message: __('passwords.invalid_code'));
+            }
+
+            if (! empty($request->password)) {
+                DB::beginTransaction();
+                $this->coach->where('email', $request->email)->first()
+                    ->update(['password' => $request->password]);
+                $resetCode->delete();
+                DB::commit();
+
+                return $this->successResponse(message: __('passwords.reset'));
+            }
+
+            return $this->successResponse(message: __('passwords.code_is_verified'));
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            Log::error("error on store  reset password in coach, exception: {$th->getMessage()}");
+
+            return $this->failedResponse(__('An error occurred. Please try again later.'));
         }
-
-        if ($request->has('password')) {
-            $this->coach->where('email', $request->email)->first()
-                ->update(['password' => $request->password]);
-
-            return $this->successResponse(message: __('passwords.reset'));
-        }
-
-        return $this->successResponse(message: __('passwords.code_is_verified'));
     }
 }
