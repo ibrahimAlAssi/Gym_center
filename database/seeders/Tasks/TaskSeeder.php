b494c333a6a1773b9541789b40cec98090cc7dce
Ibrahim Al Assi <ibrahimalassi8@gmail.com>
2024-07-16 23:42:16 +0300

    add data for description in task

--- database/seeders/Tasks/TaskSeeder.php
+++ database/seeders/Tasks/TaskSeeder.php
@@ -20,74 +20,90 @@ class TaskSeeder extends Seeder
                 'name' => 'Lat Pull downs',
                 'number' => '15',
                 'type_id' => '1',
+                'description' => 'These exercises help strengthen the back muscles,improve posture, and support overall spine health.',
             ],
             [
                 'name' => 'Pull Up',
                 'number' => '10',
                 'type_id' => '1',
+                'description' => 'These exercises help strengthen the back muscles,improve posture, and support overall spine health.',
             ],
             [
                 'name' => 'Pull Up On Rope',
                 'number' => '10',
                 'type_id' => '1',
+                'description' => 'These exercises help strengthen the back muscles,improve posture, and support overall spine health.',
             ],
             [
                 'name' => 'Single Arm Dumbbell Rows',
                 'number' => '10',
                 'type_id' => '1',
+                'description' => 'These exercises help strengthen the back muscles,improve posture, and support overall spine health.',
             ],
             // Chest
             [
                 'name' => 'Cabel Crossovers',
                 'number' => '10',
                 'type_id' => '3',
+                'description' => 'These exercises help build strength and definition in the chest area.',
             ],
             [
                 'name' => 'Chest Flyes',
                 'number' => '10',
                 'type_id' => '3',
+                'description' => 'These exercises help build strength and definition in the chest area.',
             ],
             [
                 'name' => 'dumbal press',
                 'number' => '10',
                 'type_id' => '3',
+                'description' => 'These exercises help build strength and definition in the chest area.',
             ],
             [
                 'name' => 'Incline Bench Press',
                 'number' => '10',
                 'type_id' => '3',
+                'description' => 'These exercises help build strength and definition in the chest area.',
             ],
             [
                 'name' => 'Push Up',
                 'number' => '10',
                 'type_id' => '3',
+                'description' => 'These exercises help build strength and definition in the chest area.',
             ],
             //legs
             [
                 'name' => 'Jump Rope',
                 'number' => '10',
                 'type_id' => '2',
+                'description' => 'Leg exercises are essential for overall lower body strength and stability',
             ],
             [
                 'name' => 'Lunges',
                 'number' => '10',
                 'type_id' => '2',
+                'description' => 'Leg exercises are essential for overall lower body strength and stability',
+
             ],
             [
                 'name' => 'Squat',
                 'number' => '10',
                 'type_id' => '2',
+                'description' => 'Leg exercises are essential for overall lower body strength and stability',
+
             ],
             //Shoulder
             [
                 'name' => 'Arnold Press',
                 'number' => '10',
                 'type_id' => '4',
+                'description' => ' Shoulder exercises target the deltoid muscles and help improve shoulder strength and stability.Some popular shoulder exercises include',
             ],
             [
                 'name' => 'Overhead Press',
                 'number' => '10',
                 'type_id' => '4',
+                'description' => ' Shoulder exercises target the deltoid muscles and help improve shoulder strength and stability.Some popular shoulder exercises include',
             ],
 
         ];
