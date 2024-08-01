36e7df324022f693b990c6de4504ec0e6c3123ca
Ibrahim Al Assi <ibrahimalassi8@gmail.com>
2024-08-01 14:26:38 +0300

    test code

--- app/Src/Coach/Tasks/Requests/StoreScheduleRequest.php
+++ app/Src/Coach/Tasks/Requests/StoreScheduleRequest.php
@@ -36,8 +36,8 @@ class StoreScheduleRequest extends FormRequest
 
             'schedule_tasks' => ['required', 'min:1'],
             'schedule_tasks.*.task_id' => ['required', 'integer'],
-            // 'schedule_tasks.*.repeat' => ['required', 'integer', 'min:1'],
-            // 'schedule_tasks.*.weight' => ['nullable', 'integer'],
+            'schedule_tasks.*.repeat' => ['required', 'integer', 'min:1'],
+            'schedule_tasks.*.weight' => ['nullable', 'integer'],
 
             // 'task_ids' => ['sometimes', 'array', Rule::exists('tasks', 'id')],
             // 'task_ids.*' => ['integer', 'distinct'],
